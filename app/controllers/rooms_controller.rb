2482b71f7fbeb754f7797dbcd35caef886a6ed26
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-07-20 23:53:25 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,4 +17,8 @@ class RoomsController < ApplicationController
       def room_params
         params.require(:room).permit(:name, user_ids:[])
       end
+      
+      def index
+      end
+
     end
