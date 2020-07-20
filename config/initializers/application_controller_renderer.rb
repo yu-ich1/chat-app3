738ba5b2e40ee8283176ed8a62530d214bacbb9a
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-07-20 16:09:43 +0900

    ユーザー認証機能の実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,10 @@
-# Be sure to restart your server when you modify this file.
+class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+  end
