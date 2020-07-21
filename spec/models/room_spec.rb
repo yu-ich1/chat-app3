e8ed7f1b1646f71970e642c7c787d7e7cb4a0e92
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-07-21 17:14:49 +0900

    結合テストの実装

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,20 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Room, type: :model do
-  describe '#create' do
-    before do
-      @room = FactoryBot.build(:room)
-    end
-
-    it "nameの値が存在すれば登録できること" do
-      expect(@room).to be_valid
-    end
-
-    it "nameが空では登録できないこと" do
-      @room.name = nil
-      @room.valid?
-      expect(@room.errors.full_messages).to include("Name can't be blank")
-    end
+RSpec.describe "チャットールームの削除機能", type: :system do
+  before do
+    @room_user = FactoryBot.create(:room_user)
   end
 
+  it 'チャットルームを削除すると、関連するメッセージがすべて削除されていること' do
+    # サインインする
+    sign_in(@room_user.user)
+
+    # 作成されたチャットルームへ遷移する
+    click_on(@room_user.room.name)
+
+    # メッセージ情報を5つDBに追加する
+    FactoryBot.create_list(:message, 5, room_id: @room_user.room.id, user_id: @room_user.user.id)
+
+    # 「チャットを終了する」ボタンをクリックすることで、作成した5つのメッセージが削除されていることを期待する
+    expect{
+      find_link("チャットを終了する",  href: room_path(@room_user.room)).click
+    }.to change { @room_user.room.messages.count }.by(-5)
+
+    # ルートページに遷移されることを期待する
+    expect(current_path).to eq root_path
+  end
 end
