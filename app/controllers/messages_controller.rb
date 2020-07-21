394f6d1e8eeaef4f1b30a11b4b91171322b185c4
yu-ich1 <68319482+yu-ich1@users.noreply.github.com>
2020-07-21 12:04:46 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,12 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    if @message.save
+     if @message.save
       redirect_to room_messages_path(@room)
     else
       render :index
